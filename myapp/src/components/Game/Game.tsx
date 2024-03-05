5fb55d4ee42c7150abd3dfa440bcb9d985d16264
Anna Yeritsyan <79460134+AnnaYeritsyan@users.noreply.github.com>
2024-03-05 23:47:40 +0400

    modal application
    in modal application create answer-question  information

---
+++ myapp/src/components/Game/Game.tsx
@@ -0,0 +1,11 @@
+import { Box } from '@mui/material';
+
+        
+const Game = () => {
+    return (
+       <Box>
+        Game
+       </Box>
+    );
+};
+export default Game;
--- myapp/src/components/Home/Home.tsx
+++ myapp/src/components/Home/Home.tsx
@@ -1,3 +1,4 @@
+import Game from "components/Game/Game";
 import DbConection from "./DBConection/DBConnection";
 
         
@@ -6,6 +7,7 @@ const Home = () => {
         <div >
              hello home
              <DbConection/>
+             <Game/>
         </div>
     );
 };
